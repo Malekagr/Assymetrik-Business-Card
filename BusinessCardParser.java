cc4329160a8730a3ac16ced622250fc51779b8dd
Malekagr <maa245@pitt.edu>
2018-11-05 21:15:01 -0500

    More error catching on input file

--- BusinessCardParser.java
+++ BusinessCardParser.java
@@ -10,7 +10,15 @@ public class BusinessCardParser {
 	//gets the contact info
 	//prints out each element of the ContactInfo
 	public static void main(String[] args) throws IOException {
-		ContactInfo contactInfo = getContactInfo(args[0]);
+		String inputDoc = null;
+		try {
+			inputDoc = args[0];
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("No input file, ending process.");
+			System.exit(0);
+		}
+		ContactInfo contactInfo = getContactInfo(inputDoc);
 		System.out.println("Name: "+contactInfo.getName());
 		System.out.println("Phone: "+contactInfo.getPhoneNumber());
 		System.out.println("Email: "+contactInfo.getEmailAddress());
@@ -21,7 +29,13 @@ public class BusinessCardParser {
 	//then returns a ContactInfo object after 
 	public static ContactInfo getContactInfo(String document) throws IOException {
 		File fileDocument = new File(document);
-		BufferedReader br = new BufferedReader(new FileReader(fileDocument)); 
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(fileDocument));
+		} catch (FileNotFoundException e) {
+			System.out.println("Input file does not exist, ending process.");
+			System.exit(0);
+		} 
 		String line; 
 		List<String> lines = new ArrayList<String>();
 		while ((line = br.readLine()) != null) {
